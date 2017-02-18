b1fab3fd89ecefe7eda903e88b6e6431e31efc00
RPHLuo <Robinpluo@gmail.com>
2017-02-18 13:18:25 -0500

    restructured assignments

---
+++ Assignment2/COMP2601A2Client/app/src/main/java/edu/carleton/COMP2601/comp2601a2client/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.carleton.COMP2601.comp2601a2client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
