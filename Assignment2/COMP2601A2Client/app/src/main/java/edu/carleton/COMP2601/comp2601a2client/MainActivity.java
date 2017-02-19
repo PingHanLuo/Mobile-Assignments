9d4f611446dae3792aa8f729d1742730823575ef
RPHLuo <Robinpluo@gmail.com>
2017-02-18 21:49:16 -0500

    startd on connect

--- Assignment2/COMP2601A2Client/app/src/main/java/edu/carleton/COMP2601/comp2601a2client/MainActivity.java
+++ Assignment2/COMP2601A2Client/app/src/main/java/edu/carleton/COMP2601/comp2601a2client/MainActivity.java
@@ -2,6 +2,11 @@ package edu.carleton.COMP2601.comp2601a2client;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import edu.carleton.COMP2601.comp2601a2client.common.messaging.Message;
 import edu.carleton.COMP2601.comp2601a2client.communication.Event;
@@ -14,23 +19,29 @@ public class MainActivity extends AppCompatActivity {
     public static final String ipAddr = "192.168.0.17";
     String ID;
     Reactor reactor;
+    ListView userlist;
+    ArrayAdapter listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userlist = (ListView)findViewById(R.id.lstUsers);
+        listAdapter = new ArrayAdapter(getApplicationContext(),R.layout.activity_main,R.id.lstUsers,new ArrayList<String>());
+        userlist.setAdapter(listAdapter);
         reactor = new Reactor();
         reactor.register("connect", new EventHandler() {
             @Override
             public void handleEvent(Event event) {
                 ID = (String)event.get(Fields.ID);
                 //fill players
+                listAdapter.addAll((ArrayList<String>)event.get("users"));
             }
         });
         reactor.register("new user", new EventHandler() {
             @Override
             public void handleEvent(Event event) {
-
+                listAdapter.add(event.get("user"));
             }
         });
         connectToServer();
@@ -40,6 +51,6 @@ public class MainActivity extends AppCompatActivity {
         //Send CONNECT_REQUEST
         Message connectMessage = new Message();
         connectMessage.header.type = "connect";
-
+        connectMessage.header.id = "bob";
     }
 }
--- Assignment2/COMP2601A2Client/tictactoeserver/src/main/java/edu/carleton/COMP2601/Server.java
+++ Assignment2/COMP2601A2Client/tictactoeserver/src/main/java/edu/carleton/COMP2601/Server.java
@@ -1,14 +1,25 @@
 package edu.carleton.COMP2601;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import edu.carleton.COMP2601.communication.Event;
 import edu.carleton.COMP2601.communication.EventHandler;
 import edu.carleton.COMP2601.communication.EventSourceImpl;
+import edu.carleton.COMP2601.communication.Fields;
 import edu.carleton.COMP2601.communication.Reactor;
 import edu.carleton.COMP2601.communication.ThreadWithReactor;
 
 public class Server {
+    public static final String ipAddr = "192.168.0.17";
+    public static final int PORT = 5001;
     private Reactor reactor;
     private EventSourceImpl es;
     private ThreadWithReactor twr;
@@ -20,7 +31,19 @@ public class Server {
         reactor.register("connect", new EventHandler() {
             @Override
             public void handleEvent(Event event) {
-
+                try {
+                    Event response = new Event("connect", es);
+                    //get hashmap of users
+                    ArrayList<String> otherUsers = new ArrayList<String>();
+                    for(Object user : ((Map)users).keySet()){
+                        otherUsers.add((String)user);
+                    }
+                    users.put((String) event.get(Fields.ID), twr);
+                    event.put("users",otherUsers);
+                    es.putEvent(response);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
             }
         });
     }
@@ -29,6 +52,19 @@ public class Server {
         ns.run();
     }
     public void run(){
-
+        ServerSocket listener;
+        try{
+            listener = new ServerSocket(PORT);
+            while(true){
+                Socket s = listener.accept();
+                InputStream is = s.getInputStream();
+                OutputStream os = s.getOutputStream();
+                es = new EventSourceImpl(is,os);
+                twr = new ThreadWithReactor(es, reactor);
+                twr.start();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
