65a513ca1b1894a7b43cd7957e975046680c2448
RPHLuo <Robinpluo@gmail.com>
2017-03-05 15:26:54 -0500

    updated files

--- Assignment2/COMP2601A2/app/src/main/java/edu/carleton/COMP2601/communication/EventSource.java
+++
@@ -1,8 +0,0 @@
-package edu.carleton.COMP2601.communication;
-
-import java.io.IOException;
-
-public interface EventSource {
-	public Event getEvent() throws IOException, ClassNotFoundException;
-	public void putEvent(Event e) throws IOException, ClassNotFoundException;
-}
